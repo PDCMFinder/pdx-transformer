37cb2d2c04b1835e7886cd6c8e600b7671ec4fd2
sprintell <abayomi@sprintell.org>
2020-07-12 22:52:32 +0100

    Add project to retrieve few columns from histology table

--- data-repository/src/main/java/org/pdxfinder/HistologyRepository.java
+++ data-repository/src/main/java/org/pdxfinder/HistologyRepository.java
@@ -1,5 +1,6 @@
 package org.pdxfinder;
 
+import org.pdxfinder.projection.HistologyProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -12,9 +13,11 @@ public interface HistologyRepository extends JpaRepository<Histology, Integer> {
 
     Optional<Histology> findByHistologyseqnbr(String seqNumber);
 
-    @Query("SELECT his.histologyseqnbr, his.sampleseqnbr, his.tumorgradeseqnbr, his.tumorcontent, his.necrosis, " +
-            "his.stromal, his.inflammatorycellseqnbr, his.pathologynotes, his.lowmagimagemimetype, his.lowmagimagefilename, his.highmagimagefilename from Histology his")
-    List<Object[]> findAllHistology();
-
+    @Query("SELECT his.histologyseqnbr as histologyseqnbr, his.sampleseqnbr as sampleseqnbr, " +
+            " his.tumorgradeseqnbr as tumorgradeseqnbr, his.tumorcontent as tumorcontent, his.necrosis as necrosis, " +
+            " his.stromal as stromal, his.inflammatorycellseqnbr as inflammatorycellseqnbr, his.pathologynotes as pathologynotes, " +
+            " his.lowmagimagemimetype as lowmagimagemimetype, his.lowmagimagefilename as lowmagimagefilename, " +
+            " his.highmagimagefilename as highmagimagefilename from Histology his")
+    List<HistologyProjection> findAllHistology();
 
 }
