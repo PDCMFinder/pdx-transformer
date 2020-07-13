9069651910321e57b9132b114905e78f99e0a483
sprintell <abayomi@sprintell.org>
2020-07-13 11:11:47 +0100

    Add utility for handling file write and reading process

---
+++ data-services/src/main/java/org/pdxfinder/util/FileUtil.java
@@ -0,0 +1,64 @@
+package org.pdxfinder.util;
+
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
+public class FileUtil {
+
+    private FileUtil() {
+        // Never Called
+    }
+
+    public static void write(String data, String destination, Boolean shouldAppend) throws IOException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(destination, shouldAppend))) {
+            writer.append(data);
+        }
+    }
+
+    public static Object loadFixedTsvDescriptionRows() throws IOException {
+        String contents = "";
+        try (InputStream inputStream = FileUtil.class.getResourceAsStream("/templates/head_metadata-model.tsv");
+             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+            contents = reader.lines()
+                    .collect(Collectors.joining(System.lineSeparator()));
+        }
+        return contents;
+    }
+
+    public static String serializePojoToTsv(List<?> pojoList) throws IOException {
+        CsvMapper csvMapper = new CsvMapper();
+        List<Map<String, Object>> dataList = csvMapper.convertValue(pojoList, new TypeReference<List<Map<String, Object>>>(){});
+        List<List<String>> csvData = new ArrayList<>();
+        List<String> csvHead = new ArrayList<>();
+
+        AtomicInteger counter = new AtomicInteger();
+        dataList.forEach( row ->{
+            List<String> rowData = new ArrayList<>();
+            row.forEach((key,value)->{
+                rowData.add(String.valueOf(value));
+                if (counter.get() == 0){
+                    csvHead.add(key);
+                }
+            });
+            csvData.add(rowData);
+            counter.getAndIncrement();
+        });
+
+        CsvSchema.Builder builder = CsvSchema.builder();
+        csvHead.forEach(builder::addColumn);
+
+        CsvSchema  schema = builder.build().withHeader().withLineSeparator("\n").withColumnSeparator('\t');
+        return csvMapper.writer(schema).writeValueAsString(csvData);
+    }
+
+
+}
