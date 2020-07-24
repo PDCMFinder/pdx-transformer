27f6aa7fddb5e8053edfa4368142db124136fd8e
sprintell <abayomi@sprintell.org>
2020-07-24 07:46:04 +0100

    Refactor from multi-module maven to single module maven project for simplicity

--- data-model/src/main/java/org/pdxfinder/repository/HugoGeneSymbolRepo.java
+++
@@ -1,9 +0,0 @@
-package org.pdxfinder.repository;
-
-import org.pdxfinder.domain.HugoGeneSymbol;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HugoGeneSymbolRepo extends JpaRepository<HugoGeneSymbol, Integer> {
-}
--- data-model/src/main/java/org/pdxfinder/repository/VariantClassRepo.java
+++
@@ -1,9 +0,0 @@
-package org.pdxfinder.repository;
-
-import org.pdxfinder.domain.VariantClass;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface VariantClassRepo extends JpaRepository<VariantClass, Integer> {
-}
